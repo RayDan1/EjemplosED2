58770c70660e0cfa5fd3a954b0b8ceee808943e9
Raymundo Daniel Ramírez Ramos <ray.ramirez@unikino.edu.mx>
2022-05-14 11:43:34 -0700

    int numero = 9; string numeroConCero = "99"; numeroConCero = numeroConCero.PadLeft(3, '$'); Console.WriteLine(numeroConCero);

--- EjemplosED2/Program.cs
+++ EjemplosED2/Program.cs
@@ -1,2 +1,19 @@
 ﻿string nombre = " Raymundo Daniel, ";
-Console.WriteLine(nombre.Trim());
+//Console.WriteLine(nombre.Length);
+
+//if (string.IsNullOrWhiteSpace(nombre))
+//{
+//  Console.WriteLine("string vacio");
+//}
+//else
+//{
+//  Console.WriteLine(nombre);
+//}
+
+
+int numero = 9;
+string numeroConCero = "99";
+numeroConCero = numeroConCero.PadLeft(3, '$');
+Console.WriteLine(numeroConCero);
+
+  
